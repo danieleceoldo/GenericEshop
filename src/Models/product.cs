c2959519080fa2daf5400b4a0ab2b93fa865faed
Daniele Ceoldo <daniele.ceoldo@gmail.com>
2026-06-06 15:49:59 +0200

    First commit.

---
+++ src/Models/product.cs
@@ -0,0 +1,10 @@
+namespace GenericEshop.Models;
+
+public class Product
+{
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+}
